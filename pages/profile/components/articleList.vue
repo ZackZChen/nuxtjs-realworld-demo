68f15d7ccdf4b210753eee3d28b1426b00ff6de7
ZackChen <451609327@qq.com>
2021-03-18 01:15:25 +0800

    profile page

---
+++ pages/profile/components/articleList.vue
@@ -0,0 +1,79 @@
+<template>
+  <div>
+    <div class="article-preview" v-for="item in articles" :key="item.slug">
+      <div class="article-meta">
+        <nuxt-link
+          :to="{
+            name: 'profile',
+            params: {
+              username: item.author.username
+            }
+          }"
+        >
+          <img :src="item.author.image" />
+        </nuxt-link>
+        <div class="info">
+          <nuxt-link
+            :to="{
+              name: 'profile',
+              params: {
+                username: item.author.username
+              }
+            }"
+            class="author"
+          >
+            {{ item.author.username }}
+          </nuxt-link>
+          <span class="date">{{ item.createdAt | date('MMMM DD, YYYY') }}</span>
+        </div>
+        <button
+          class="btn btn-outline-primary btn-sm pull-xs-right"
+          :class="{ active: item.favorited }"
+          :disabled="item.favoriteDisabled"
+          @click="onFavoriteClick(item)"
+        >
+          <i class="ion-heart"></i> {{ item.favoritesCount }}
+        </button>
+      </div>
+      <nuxt-link
+        :to="{ name: 'article', params: { slug: item.slug } }"
+        class="preview-link"
+      >
+        <h1>{{ item.title }}</h1>
+        <p>{{ item.description }}</p>
+        <span>Read more...</span>
+      </nuxt-link>
+    </div>
+  </div>
+</template>
+
+<script>
+import { addFavoriteAPI, removeFavoriteAPI } from '@/api/article'
+
+export default {
+  name: 'ArticleList',
+  props: {
+    articles: {
+      type: Array,
+      default: () => []
+    }
+  },
+  methods: {
+    async onFavoriteClick(article) {
+      article.favoriteDisabled = true
+      if (article.favorited) {
+        await removeFavoriteAPI(article.slug)
+        article.favorited = false
+        article.favoritesCount += -1
+      } else {
+        await addFavoriteAPI(article.slug)
+        article.favorited = true
+        article.favoritesCount += 1
+      }
+      article.favoriteDisabled = false
+    }
+  }
+}
+</script>
+
+<style></style>
--- pages/profile/index.vue
+++ pages/profile/index.vue
@@ -4,15 +4,14 @@
       <div class="container">
         <div class="row">
           <div class="col-xs-12 col-md-10 offset-md-1">
-            <img src="http://i.imgur.com/Qr71crq.jpg" class="user-img" />
-            <h4>Eric Simons</h4>
+            <img :src="user.image" class="user-img" />
+            <h4>{{ user.username }}</h4>
             <p>
-              Cofounder @GoThinkster, lived in Aol's HQ for a few months, kinda
-              looks like Peeta from the Hunger Games
+              {{ user.bio }}
             </p>
             <button class="btn btn-sm btn-outline-secondary action-btn">
               <i class="ion-plus-round"></i>
-              &nbsp; Follow Eric Simons
+              &nbsp; Follow {{ user.username }}
             </button>
           </div>
         </div>
@@ -25,56 +24,37 @@
           <div class="articles-toggle">
             <ul class="nav nav-pills outline-active">
               <li class="nav-item">
-                <a class="nav-link active" href="">My Articles</a>
+                <nuxt-link
+                  :class="['nav-link', { active: tab !== 'favorited_article' }]"
+                  exact
+                  :to="{
+                    name: 'profile',
+                    params: {
+                      username: user.username
+                    }
+                  }"
+                >
+                  My Articles
+                </nuxt-link>
               </li>
               <li class="nav-item">
-                <a class="nav-link" href="">Favorited Articles</a>
+                <nuxt-link
+                  :class="['nav-link', { active: tab === 'favorited_article' }]"
+                  exact
+                  :to="{
+                    name: 'profile',
+                    params: {
+                      username: user.username
+                    },
+                    query: { tab: 'favorited_article' }
+                  }"
+                >
+                  Favorited Articles
+                </nuxt-link>
               </li>
             </ul>
           </div>
-
-          <div class="article-preview">
-            <div class="article-meta">
-              <a href=""><img src="http://i.imgur.com/Qr71crq.jpg"/></a>
-              <div class="info">
-                <a href="" class="author">Eric Simons</a>
-                <span class="date">January 20th</span>
-              </div>
-              <button class="btn btn-outline-primary btn-sm pull-xs-right">
-                <i class="ion-heart"></i> 29
-              </button>
-            </div>
-            <a href="" class="preview-link">
-              <h1>How to build webapps that scale</h1>
-              <p>This is the description for the post.</p>
-              <span>Read more...</span>
-            </a>
-          </div>
-
-          <div class="article-preview">
-            <div class="article-meta">
-              <a href=""><img src="http://i.imgur.com/N4VcUeJ.jpg"/></a>
-              <div class="info">
-                <a href="" class="author">Albert Pai</a>
-                <span class="date">January 20th</span>
-              </div>
-              <button class="btn btn-outline-primary btn-sm pull-xs-right">
-                <i class="ion-heart"></i> 32
-              </button>
-            </div>
-            <a href="" class="preview-link">
-              <h1>
-                The song you won't ever stop singing. No matter how hard you
-                try.
-              </h1>
-              <p>This is the description for the post.</p>
-              <span>Read more...</span>
-              <ul class="tag-list">
-                <li class="tag-default tag-pill tag-outline">Music</li>
-                <li class="tag-default tag-pill tag-outline">Song</li>
-              </ul>
-            </a>
-          </div>
+          <article-list :articles="articles" />
         </div>
       </div>
     </div>
@@ -82,9 +62,42 @@
 </template>
 
 <script>
+import { mapState } from 'vuex'
+import ArticleList from './components/articleList'
+import { getArticlesAPI } from '@/api/article'
+
 export default {
   middleware: 'authenticated',
-  name: 'ProfilePage'
+  name: 'ProfilePage',
+  async asyncData({ query, store }) {
+    const page = +(query.page || 1)
+    const limit = 20
+    const { tab = 'my_articles' } = query
+    const apiParams = {
+      limit,
+      offset: (page - 1) * limit
+    }
+    if (tab === 'favorited_article') {
+      apiParams.favorited = store.state.user.username
+    } else {
+      apiParams.author = store.state.user.username
+    }
+    const articleRes = await getArticlesAPI(apiParams)
+    const { articles, articlesCount } = articleRes.data
+    articles.forEach((i) => (i.favoriteDisabled = false))
+    return {
+      articles,
+      articlesCount,
+      limit,
+      page,
+      tab
+    }
+  },
+  watchQuery: ['tab'],
+  components: { ArticleList },
+  computed: {
+    ...mapState(['user'])
+  }
 }
 </script>
 
